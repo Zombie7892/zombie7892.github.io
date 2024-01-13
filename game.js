aa5d4388564d2e08291e7cbda86e5b397e68d953
Kirill Maksimovich <warznether2@yandex.ru>
2024-01-13 15:23:05 +0300

    Release 1: return function

--- game.js
+++ game.js
@@ -2,12 +2,18 @@ const greetingField = document.getElementById('greeting');
 const playerScore = document.getElementById('score');
 const playerPoints = document.getElementById('points');
 const stopBtn = document.querySelector('.stop')
+const returnBtn = document.querySelector('.return')
 const card = document.querySelector('.square')
 console.log(card);
 
 greetingField.innerText = `Добро пожаловать на испытание, ${localStorage.getItem('currentPlayer')} !`
 stopBtn.addEventListener('click',() =>{
-    window.open('./index.html','_self');
+    alert(`Вы набрали ${playerPoints.innerText} очков!`)
+    playerPoints.innerText = 0;
+})
+
+returnBtn.addEventListener('click',() => {
+    window.open('./index.html','_self')
 })
 
 function changeColor() {
